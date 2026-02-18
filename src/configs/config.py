562fc136e63bc7b7c049c24bbc7f490f0918bc7d
carloscalvin <carloscalvinp@gmail.com>
2026-02-18 18:46:53 +0100

    Exp 28: regnety_008.pycls_in1k model

--- src/configs/config.py
+++ src/configs/config.py
@@ -5,7 +5,7 @@ import os
 cfg = SimpleNamespace(**{})
 
 cfg.project_name = "BirdCLEF2026"
-cfg.exp_name = "tf_efficientnet_b0_ns_union_mixup_run27"
+cfg.exp_name = "regnety_008.pycls_in1k_union_mixup_run28"
 cfg.num_workers = 0
 cfg.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cfg.seed = 42
@@ -62,11 +62,11 @@ cfg.spec_aug_time_mask = 0
 cfg.spec_aug_freq_mask = 0
 cfg.spec_aug_prob = 0.0
 
-cfg.gaussian_noise_prob = 0.0
+cfg.gaussian_noise_prob = 0.2
 cfg.gaussian_noise_limit = (0.5, 2.0)
 
 model_cfg = SimpleNamespace(**{})
-model_cfg.model_name = "tf_efficientnet_b0_ns"
+model_cfg.model_name = "regnety_008.pycls_in1k"
 model_cfg.pretrained = True
 model_cfg.num_classes = 0
 model_cfg.ema_decay = 0.999
