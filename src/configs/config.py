310128924d71882ed7d76ce33f12ba90da7d5526
carloscalvin <carloscalvinp@gmail.com>
2026-02-19 13:22:46 +0100

    Exp 29 model eca_nfnet_l0.ra2_in1k

--- src/configs/config.py
+++ src/configs/config.py
@@ -5,7 +5,7 @@ import os
 cfg = SimpleNamespace(**{})
 
 cfg.project_name = "BirdCLEF2026"
-cfg.exp_name = "regnety_008.pycls_in1k_union_mixup_run28"
+cfg.exp_name = "eca_nfnet_l0.ra2_in1k_union_mixup_run29"
 cfg.num_workers = 0
 cfg.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cfg.seed = 42
@@ -66,7 +66,7 @@ cfg.gaussian_noise_prob = 0.2
 cfg.gaussian_noise_limit = (0.5, 2.0)
 
 model_cfg = SimpleNamespace(**{})
-model_cfg.model_name = "regnety_008.pycls_in1k"
+model_cfg.model_name = "eca_nfnet_l0.ra2_in1k"
 model_cfg.pretrained = True
 model_cfg.num_classes = 0
 model_cfg.ema_decay = 0.999
